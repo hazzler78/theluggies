dbb01e83fa68809de6ee49ad3a385ed941f96566
Mikael <hazzler@gmail.com>
2025-10-07 08:55:09 +0200

    fix(build): remove next-intl routing exports to satisfy Cloudflare build

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,13 +1,4 @@
-import {defineRouting} from 'next-intl/routing';
-
 export const locales = ['en', 'sv'] as const;
 export type AppLocale = typeof locales[number];
-
-export const routing = defineRouting({
-  locales,
-  defaultLocale: 'en'
-});
-
-export const {Link, getPathname, redirect, usePathname, useRouter} = routing;
 
 
